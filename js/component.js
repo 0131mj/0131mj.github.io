a4cacc495da93b743ad6f0da21b7ddcd7dbcb906
mjbae <0131mj@gmail.com>
2022-09-12 19:44:32 +0900

    - 프로필 모달 메뉴 노출처리

--- js/component.js
+++
@@ -1,9 +0,0 @@
-// 등록
-Vue.component('footer-component', {
-    template: '<div class="wrapper"><p>(c)copyright 2018 by Bae Myoung-Jin</p></div>'
-})
-
-// 루트 인스턴스 생성
-new Vue({
-    el: 'footer'
-})
